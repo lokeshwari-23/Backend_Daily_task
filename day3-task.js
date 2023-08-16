907b9526a32dab8148b2e25354bdd95ee251390d
lokeshwari-23 <134676662+lokeshwari-23@users.noreply.github.com>
2023-08-16 13:03:29 +0530

    Add files via upload

--- day3-task.js
+++ day3-task.js
@@ -1,143 +1,40 @@
-//===============================================================================1
-//fetch the data from a collections and write it to a text file
 
-const express = require('express');
+//================================================================1
+const bodyParser=require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
+app.post('/',(req,res)=>{
+    var name=req.body.name;
+    var salary=req.body.salary;
+    if(!name && !salary){
+        res.status(404).send('error')
+    }else{
+        res.status(200).send('success')
+    }
 
-const url="mongodb://0.0.0.0:27017/task"
-const client=new MongoClient(url);
-const fs=require('fs');
-const app = express();
-
-client.connect(url).then(()=>
-{
-    const DB=client.db("task");
-    const coll=DB.collection("task1");
-    coll.find({}).toArray().then((val)=>{
+})
 
 
-    fs.writeFile('data.txt',JSON.stringify(val),(err,res)=>{
-        if(err){
-            console.log(err);
+    
+//======================================================================2
+app.post("/search/:name/:age", (req, res) => {
+        const name = req.params.name;
+        const age = req.params.age;
+    
+        const isAlpha = /^[a-zA-Z]+$/.test(name); 
+        const isNumeric = /^[0-9]+$/.test(age); 
+    
+        if (!isAlpha) {
+            res.send("Enter only alphabetic characters for the name.");
+        } else if (!isNumeric) {
+            res.send("Enter only numeric characters for the age.");
+        } else {
+            res.send(`Name: ${name}, Age: ${age}`);
         }
-        else{
-             console.log("respect")
-        }
-    })
-})
-
-
-})
-app.listen(8080,()=>{
-    console.log("listening on port 8080!!!!!!!!!!!");
-})
-
-//=======================================================================1
-
-
-
-
-const express = require('express');
-const  MongoClient = require('mongodb').MongoClient;
-const  ObjectId = require('mongodb').ObjectId;
-
-const url = "mongodb://0.0.0.0:27017/task";
-const client = new MongoClient(url);
-const app = express();
-
-client.connect().then(() => {
-    console.log("Connected to Mongodb");
-    const db = client.db("task");
-    const coll = db.collection('task2');
-
-
-    app.get('/create/:name/:salary', (req, res) => {
-        
-        var Name = req.params.name;
-        var Salary = req.params.salary;
-        const obj = { name: Name, salary: Salary};
-        coll.insertOne(obj).then(() => {
-            console.log("Inserted successfully");
-        });
-        res.send("Homepage1");
     });
 
-    app.get('/read', (req, res) => {
-        res.send("Homepage2");
-        const obj_id = new ObjectId("64d9f9f26320f36e3f58899c");
-        coll.find({ _id: obj_id }).toArray().then((val) => {
-            console.log(val);
-        });
-    });
 
-    app.get('/update', (req, res) => {
-        res.send("Homepage3");
-        const obj_id = new ObjectId("64d9f9f26320f36e3f58899c");
-        coll.updateOne(
-            { _id: obj_id },
-            { $set: { name: "karthi" } }
-        ).then(() => {
-            console.log("Updated");
-        });
-    });
-
-    app.get('/delete', (req, res) => {
-        res.send("Homepage4");
-        const obj_id = new ObjectId("64d9f9f26320f36e3f58899c");
-        coll.deleteOne({ _id: obj_id }).then(() => {
-            console.log("Deleted");
-        });
-    });
-
-    app.listen(3000, () => {
-        console.log('Server is running!');
-    });
-}).catch(err => {
-    console.error('err', err);
-});
-
-
-// write a MongoDB query to find the higesht score for restaurants
-//db.restaurants.aggregate([{$group:{_id:"$name",max:{$max:{$max:"$grades.score"}}}}])
-
-//===========================================================================================
-
-//write a MongoDB query to find the lowest score for each restaurants
-//db.restaurants.aggregate([{$group:{_id:"$name",min:{$min:{min:{$min:"$grades.score"}}}}}])
-
-//==========================================================================================
-
-//write a MongoDB query to find the count of restaurants in each borough
-//db.restaurants.aggregate({$group:{_id:"$borough",sum:{$sum:1}}})
-
-//===========================================================================================
-
-//write a MongoDB query to find the count of restaurants that receive grade of A for each cuisine.
-//db.restaurants.aggregate({$match:{"grades.grade":"A"}},{$group:{_id:"$cuisine",sum:{$sum:1}}})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.listen(8080,()=>{
+        console.log('server is running');
+        })
